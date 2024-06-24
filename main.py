12a4587ad0d2cc8dcaa2fb85a92e691ac98743ec
Apis_Carnica <nathan@artiotech.org>
2024-06-24 02:09:57 +0000

    Fixed some DB code.

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 import random
 from datetime import datetime, timedelta
 import discord
-from dotenv import load_dotenv
 import requests
 import ssl
 import mysql.connector
@@ -17,32 +16,28 @@ hours = 0
 messages = 0
 activities = 0
 
-url = requests.get("https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=<your client id goes here>&redirect_uri=http://localhost:3000&scope=channel%3Amanage%3Apolls+channel%3Aread%3Apolls&state=c3ab8aa609ea11e793ae92361f002671")
-
-
-load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD_ID = os.getenv('GUILD_ID')
 DB_USER = os.getenv('DB_USER')
-DB_PASS = os.getenv('DB_PASSWD')
+DB_PASS = os.getenv('DB_PASS')
 
 
 def usersConnect():
     return mysql.connector.connect(
+        auth_plugin='auth_socket',
         host='localhost',
         user=DB_USER,
         password=DB_PASS,
         database="bnb")
 
-def usersSetRecord(t_id, d_id, points, hours, messages, activities):
+def usersSetRecord(t_id: str, d_id: str, points: int, hours: int, messages: int, activities: int):
     """
     Takes the variables and sets them as a record in the users table if the
     email isn't in the verified table
     """
     conx = usersConnect()
     cursor = conx.cursor()
-    sql = "INSERT INTO users (t_id, d_id, points, hours, messages, activities) VALUES (%s, %s, %s, %s)"
+    sql = "INSERT INTO users (t_id, d_id, points, hours, messages, activities) VALUES (%s, %s, %s, %s, %s, %s)"
     val = (t_id, d_id, points, hours, messages, activities)
     cursor.execute(sql, val)
     conx.commit()
@@ -70,7 +65,9 @@ def usersDeleteRecord(t_id: str):
     cursor.execute(sql, val)
     conx.commit()
 
-client = discord.Client()
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 
 
 @client.event
@@ -80,69 +77,12 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.channel.id == "1253253393913479199":
+    if message.channel.id == 1253253393913479199:
         if message.content.startswith('$link'):
             t_id = message.content.split(' ')[-1]
-            if t_id not null:
-                usersSetRecord(t_id, message.author.id, 0, 0, 0, 0)
-            elif t_id == '':
-                await message.channel.send("Make sure to give us your Twitch username with the link command!")
-    
+            print(t_id)
+            usersSetRecord(t_id, str(message.author.id), 0, 0, 0, 0)
+            await message.channel.send("All done!")
 
-"""
-@client.event
-async def on_message(message):
-    if message.channel.recipient == message.users:
-        if message.content.startswith('!email'):
-            d_id = message.content.split(' ')[-1]
-            code = []
-            for _ in range(6):
-                code.append(str(random.randint(0,9)))
-            code = ''.join(code)
-            expiry = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S")
-            d_id = str(message.channel.recipient)
-
-            if bool(check_verified(t_id)):
-                set_record(t_id, d_id, code, expiry)
-                send_email(code, t_id)
-                await message.channel.send("An email was sent to the address you provided. If you have trouble finding it, try refreshing your browser, wait a few minutes or check your spam folder. Feel free to reach out to **Ursa#1337** with any questions you have.")
-
-            else:
-                await message.channel.send("That email has already been verified. If you think message in error, please make a post in tech-support so our moderators can assist you.")
-
-        elif message.content.startswith('!verify'):
-            code = message.content.split(' ')[-1]
-            d_id = str(message.channel.recipient)
-
-            if bool(check_record(code, d_id)):
-                set_verified(d_id)
-                delete_record(d_id)
-
-                guild = client.get_guild(int(GUILD_ID))
-                print(guild.id)
-                role = discord.utils.get(guild.roles, name='valid')
-                print(role.name)
-                member = discord.utils.find(lambda m : m.id == message.channel.recipient.id, guild.members)
-                print(member.id)
-                if role is not None:
-                    if member is not None:
-                        await member.add_roles(role)
-                        await message.channel.send("You're all set! We look forward to learning with you!")
-                    else:
-                        print("Member doesn't exist.")
-                else:
-                    print("Role doesn't exist or is misconfigured in '.env'.")
-            else:
-                await message.channel.send("It looks like your code is wrong, please try again.")
-
-        elif message.content.startswith('!delete'):
-            field = message.content.split(' ')[-1]
-
-            delete_record(field)
-            message.channel.send("We'll get that taken care of for you!")
-
-        else:
-            pass
-"""
 
 client.run(TOKEN)
