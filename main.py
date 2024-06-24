0e65d71e8d490a4c91b4d2284b0ccf4fff42046e
Apis_Carnica <nathan@artiotech.org>
2024-06-23 22:33:38 -0400

    Wow, I'm so dumb.

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 
 
-def usersConnect():
+def dbConnect():
     return mysql.connector.connect(
         auth_plugin='auth_socket',
         host='localhost',
@@ -30,12 +30,57 @@ def usersConnect():
         password=DB_PASS,
         database="bnb")
 
+def sheetCreate(t_id: str, d_id: str, points: int, hours: int, messages: int, activities: int):
+    """
+    Takes the variables and sets them as a record in the users table if the
+    email isn't in the verified table
+    """
+    conx = dbConnect()
+    cursor = conx.cursor()
+    sql = "INSERT INTO users (t_id, d_id, points, hours, messages, activities) VALUES (%s, %s, %s, %s, %s, %s)"
+    val = (t_id, d_id, points, hours, messages, activities)
+    cursor.execute(sql, val)
+    conx.commit()
+
+def sheetFind(id: str) -> list:
+    """
+    Sends a SELECT query from the users server and verifies the variables.
+    Returns True if the code supplied matches the one populating the record.
+    """
+    conx = dbConnect()
+    cursor = conx.cursor()
+    sql = "SELECT * FROM users WHERE t_id = %s"
+    val = (t_id)
+    cursor.execute(sql, val)
+    result = cursor.fetchall()
+    return result
+
+def sheetDelete(t_id: str):
+    "Deletes the selected table from the table"
+    # Check for symbols to qualify index type: d_id -> email
+    conx = dbConnect()
+    cursor = conx.cursor()
+    sql = "DELETE FROM users WHERE t_id = %s"
+    val = (t_id)
+    cursor.execute(sql, val)
+    conx.commit()
+
+
+def sheetUpdate(id: str):
+    conx = dbConnect()
+    cursor = conx.cursor()
+    sql = "UPDATE sheets SET id = %s, " # put the columns in lazy.
+    val = (id, etc.)
+    cursor.exesute(sql, val)
+    conx.commit()
+
+
 def usersSetRecord(t_id: str, d_id: str, points: int, hours: int, messages: int, activities: int):
     """
     Takes the variables and sets them as a record in the users table if the
     email isn't in the verified table
     """
-    conx = usersConnect()
+    conx = dbConnect()
     cursor = conx.cursor()
     sql = "INSERT INTO users (t_id, d_id, points, hours, messages, activities) VALUES (%s, %s, %s, %s, %s, %s)"
     val = (t_id, d_id, points, hours, messages, activities)
@@ -47,7 +92,7 @@ def usersCheckRecord(t_id: str) -> list:
     Sends a SELECT query from the users server and verifies the variables.
     Returns True if the code supplied matches the one populating the record.
     """
-    conx = usersConnect()
+    conx = dbConnect()
     cursor = conx.cursor()
     sql = "SELECT * FROM users WHERE t_id = %s"
     val = (t_id)
@@ -58,7 +103,7 @@ def usersCheckRecord(t_id: str) -> list:
 def usersDeleteRecord(t_id: str):
     "Deletes the selected table from the table"
     # Check for symbols to qualify index type: d_id -> email
-    conx = usersConnect()
+    conx = dbConnect()
     cursor = conx.cursor()
     sql = "DELETE FROM users WHERE t_id = %s"
     val = (t_id)
