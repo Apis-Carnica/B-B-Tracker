6cc01d20b5ada4ee1b17e4693277f280b03ad912
Apis_Carnica <nathan@artiotech.org>
2024-06-24 14:03:46 -0400

    More changes

--- main.py
+++ main.py
@@ -3,23 +3,22 @@
 import os
 import random
 from datetime import datetime, timedelta
+import dotenv
+from dotenv import load_dotenv
 import discord
+from discord import app_commands
+from discord.ext import commands
 import requests
 import ssl
 import mysql.connector
 
 
-t_id = ''
-d_id = ''
-points = 0
-hours = 0
-messages = 0
-activities = 0
+load_dotenv(dotenv.find_dotenv())
 
 
-TOKEN = os.getenv('DISCORD_TOKEN')
-DB_USER = os.getenv('DB_USER')
-DB_PASS = os.getenv('DB_PASS')
+TOKEN = os.environ['DISCORD_TOKEN']
+DB_USER = os.environ['DB_USER']
+DB_PASS = os.environ['DB_PASS']
 
 
 def dbConnect():
@@ -30,38 +29,31 @@ def dbConnect():
         password=DB_PASS,
         database="bnb")
 
-def sheetCreate(t_id: str, d_id: str, points: int, hours: int, messages: int, activities: int):
-    """
-    Takes the variables and sets them as a record in the users table if the
-    email isn't in the verified table
-    """
+
+# Sheets table db operations
+
+def sheetCreate(id: str,):
     conx = dbConnect()
     cursor = conx.cursor()
-    sql = "INSERT INTO users (t_id, d_id, points, hours, messages, activities) VALUES (%s, %s, %s, %s, %s, %s)"
-    val = (t_id, d_id, points, hours, messages, activities)
+    sql = "INSERT INTO sheets (id) VALUES (%s)"
+    val = (id)
     cursor.execute(sql, val)
     conx.commit()
 
 def sheetFind(id: str) -> list:
-    """
-    Sends a SELECT query from the users server and verifies the variables.
-    Returns True if the code supplied matches the one populating the record.
-    """
     conx = dbConnect()
     cursor = conx.cursor()
-    sql = "SELECT * FROM users WHERE t_id = %s"
-    val = (t_id)
+    sql = "SELECT * FROM sheets WHERE id = %s"
+    val = (id)
     cursor.execute(sql, val)
     result = cursor.fetchall()
     return result
 
-def sheetDelete(t_id: str):
-    "Deletes the selected table from the table"
-    # Check for symbols to qualify index type: d_id -> email
+def sheetDelete(id: str):
     conx = dbConnect()
     cursor = conx.cursor()
-    sql = "DELETE FROM users WHERE t_id = %s"
-    val = (t_id)
+    sql = "DELETE FROM sheets WHERE id = %s"
+    val = (id)
     cursor.execute(sql, val)
     conx.commit()
 
@@ -70,10 +62,12 @@ def sheetUpdate(id: str):
     conx = dbConnect()
     cursor = conx.cursor()
     sql = "UPDATE sheets SET id = %s, " # put the columns in lazy.
-    val = (id, etc.)
+    val = (id)
     cursor.exesute(sql, val)
     conx.commit()
 
+
+# Users table db operations
 
 def usersSetRecord(t_id: str, d_id: str, points: int, hours: int, messages: int, activities: int):
     """
@@ -111,28 +105,33 @@ def usersDeleteRecord(t_id: str):
     conx.commit()
 
 
-intents = discord.Intents.all()
-client = discord.Client(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix = '$', intents = discord.Intents.all())
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print("Logged on as {}".format(client.user))
+    print(f"Logged on as {bot.user.name}")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands.")
+    except Exception as e:
+        print(e)
 
 
-@client.command()
-async def erm():
-    erm = os.getenv('ERM')
-    os.environ['ERM'] = str(int(erm) + 1)
-    await message.channel.send("Tigerfart has said 'erm' {} times!".format(os.getenv('ERM')))
+@bot.tree.command(name='erm')
+async def erm(interaction: discord.Interaction):
+    erm = os.environ['ERM']
+    dotenv.set_key(dotenv.find_dotenv(), "ERM", str(int(erm) + 1))
+    await interaction.response.send_message(f"Tigerfae has said 'erm' {erm} times!")
 
 
-@client.command()
-async def link(t_id):
-        usersSetRecord(t_id, str(message.author.id), 0, 0, 0, 0)
-        await message.channel.send("You've been added, {}!".format(t_id))
+@bot.tree.command(name='link')
+@app_commands.describe(t_id = 'Your Twitch account name')
+async def link(interaction: discord.Interaction, t_id: str):
+        usersSetRecord(t_id, str(interaction.user.id), 0, 0, 0, 0)
+        await interaction.response.send_message(f"You've successfully tied the user `{t_id}` to your account, {interaction.user.name}!")
 
 
 #@client.command()
 
-client.run(TOKEN)
+bot.run(TOKEN)
